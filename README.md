d5e41b743a956064cb79b3e4c7507354f5c5e004
leokun <leokun@leokundeMac-Pro.local>
2023-12-19 23:17:19 +0800

    showLog false

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ uniapp vue3小程序模板
 
 - cli模式，在任意编辑器中使用
 - 集成eslint，prettier没有冲突，提交自动格式化代码
-- 集成 unocss 
+- 集成 unocss
 - 集成 less
 - meta-page模式,类型安全
 - 集成 auto-import, 无需引入自动导入vue的函数、src/hooks、src/components
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => {
     plugins: [
       UniPages(
         presetPageMeta({
-          showLog: true,
+          showLog: false,
           mergePages: true,
           configSource: (resolve) => resolve('pages.config.ts'),
           dts: 'types/page.d.ts'
